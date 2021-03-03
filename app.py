9aeef12f5f5dc68dacd88914172dfa0550e92cc9
Slimer210 <66945259+Slimer210@users.noreply.github.com>
2021-03-03 17:48:22 +0800

    Flask app 1.0

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def err_not_found(e):
+    return render_template("notfound.html") 
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
